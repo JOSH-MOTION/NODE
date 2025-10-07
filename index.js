a436f6f624e031c31056930dc403c1fc96244e81
JOSH-MOTION <joshuadoe168@gmail.com>
2025-10-07 16:11:47 +0000

    week 1

--- index.js
+++ index.js
@@ -10,14 +10,27 @@ const Home = (req,res)=>{
     res.send("hello this is the home page")
 }
 
+const Services = (req,res)=>{
+     res.send("hello this is the About page")
+}
+const Contact = (req,res)=>{
+     res.send("hello this is the About page")
+}
 const About = (req,res)=>{
+     res.send("hello this is the About page")
+}
+const Products = (req,res)=>{
      res.send("hello this is the About page")
 }
 
 
 // routes
-server.use('/about',About)
-server.get('/',Home)
+server.get('/product',Products)
+server.patch('/services',Services)
+server.put('/contact',Contact)
+server.post('/about',About)
+server.use('/',Home)
+
 
 
 // initialize
